1f9e78c849d27492007324828df31947c5908331
abdumutal <abdumutalmahamatqulov6@gmail.com>
2025-04-04 13:55:42 +0500

    tailwind

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div className='flex justify-center items-center'>
+        <h1 className='text-red-500 justify-center flex items-center '>Tailwind</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
